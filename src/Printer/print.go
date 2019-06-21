f469a5a51ce694dbced8a37f273dc4321391690a
Nikki Bergamini <nikkibergamini@gmail.com>
2019-06-21 13:09:34 -0400

    can take a dot and convert to ascii art in console with 2 characters

--- src/Printer/print.go
+++ src/Printer/print.go
@@ -2,29 +2,50 @@ package main
 
 import (
     "fmt"
-    // "os"
+    "os"
 	"log"
-	"io/ioutil"
+	// "io/ioutil"
 	// "strconv"
 	// "reflect"
+	"image/jpeg"
+	"image/color"
 )
 
 
 
 func main() {
-	i, err := ioutil.ReadFile("../AsciiArt/RubberDuck.txt")
-	sz := len(i)
-	
-	
-	
+	i, err := os.Open("../AsciiArt/dot.jpg")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Err in open: ", err)
+	}
+	imageData, err := jpeg.Decode(i)
+	if err != nil {
+		log.Fatal("Err in image: ", err)
 	}
 
-	l := 0 
-	for  l < sz {
-		fmt.Print(string(i[l]))
-		l++
+	// var size int64 = len([]imageData)
+	// fmt.Print("imageData: ", imageData.Bounds())
+	// fmt.Print("imageData: ", imageData.At(0,0))
+	// fmt.Print("imageType: ", imageType)
+	// fileInfo, _ := i.Stat()
+  	// var size int64 = fileInfo.Size()
+	// bytes := make([]byte, size)
+
+
+	// fmt.Print(imageData)
+
+	
+	
+	for y := imageData.Bounds().Min.Y; y < imageData.Bounds().Max.Y; y++ {
+		for x := imageData.Bounds().Min.X; x < imageData.Bounds().Max.X; x++ {
+			c := color.GrayModel.Convert(imageData.At(x,y)).(color.Gray)
+			if  c.Y >  50 {
+				fmt.Print("  ")
+			} else {
+				fmt.Print("**")
+			}
+		}
+		fmt.Print("\n")
 	}
 	
 }
